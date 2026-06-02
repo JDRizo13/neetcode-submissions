4c61e6d3eb70e2787d32b65de05fd3937b9fca4d
Juan Rizo <juand_rizo@berkeley.edu>
2026-06-01 17:04:13 -0700

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        mid = len(nums) // 2
+        leftHalf = nums[:mid]
+        rightHalf = nums[mid:]
+
+        leftSorted = self.sortArray(leftHalf)
+        rightSorted = self.sortArray(rightHalf)
+
+        result = []
+        leftIndex = 0
+        rightIndex = 0
+        while leftIndex < len(leftSorted) and rightIndex < len(rightSorted):
+            if leftSorted[leftIndex] < rightSorted[rightIndex]:
+                result.append(leftSorted[leftIndex])
+                leftIndex += 1
+            else:
+                result.append(rightSorted[rightIndex])
+                rightIndex += 1
+        while leftIndex < len(leftSorted):
+            result.append(leftSorted[leftIndex])
+            leftIndex += 1
+        while rightIndex < len(rightSorted):
+            result.append(rightSorted[rightIndex])
+            rightIndex += 1
+        return result
